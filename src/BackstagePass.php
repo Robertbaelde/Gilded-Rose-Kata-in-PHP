4203d023e3369f38f08ee184fed895727b956ba4
Robert Baelde <robert_baelde@hotmail.com>
2021-01-11 12:10:39 +0100

    Refactor backstage pass sellIn

--- src/BackstagePass.php
+++ src/BackstagePass.php
@@ -8,25 +8,24 @@ class BackstagePass extends GildedRose
 {
     public function tick()
     {
+        $this->sellIn--;
 
-        if ($this->sellIn <= 0) {
+        if ($this->sellIn < 0) {
             $this->quality = 0;
-            $this->sellIn--;
             return;
         }
 
         $qualityIncrease = 1;
 
-        if ($this->sellIn < 11) {
+        if ($this->sellIn < 10) {
             $qualityIncrease++;
         }
 
-        if ($this->sellIn < 6) {
+        if ($this->sellIn < 5) {
             $qualityIncrease++;
         }
 
         $this->quality = min($this->quality + $qualityIncrease, 50);
 
-        $this->sellIn--;
     }
 }
